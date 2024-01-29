8f344a80f0cd6ab14a5e33b610e10dc600596e50
sinistergeek <susanbhattraidon@gmail.com>
2024-01-29 17:57:41 +0545

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+from lexer import Lexer
+from parser_ import Parser
+from interpreter import Interpreter
+import sys
+while True:
+    text = input("calc > ")
+    if text.strip() == "":
+        continue
+    try:
+        lexer =Lexer(text)
+        tokens =lexer.get_tokens()
+        parser = Parser(tokens)
+        tree = parser.parse()
+
+        if tree is None:
+            continue
+        interpreter = Interpreter()
+        value = interpreter.visit(tree)
+        print(value)
+    except RuntimeError as e:
+        print("error:",e.args[0],file = sys.stderr)
